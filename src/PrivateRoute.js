8becc7034eac36b187b109ec280d66a3dc9a6f95
Josh Valentino <josh3274@gmail.com>
2023-02-27 00:52:17 -0600

    build

---
+++ src/PrivateRoute.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate, Route } from "react-router-dom";
+
+function PrivateRoute({ children }) {
+    const isAuthenticated = localStorage.getItem("isAuthenticated");
+
+    return (
+        isAuthenticated ? children : <Navigate to="/" />
+    );
+}
+
+export default PrivateRoute;
